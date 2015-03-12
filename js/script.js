9f8e287159e3c4a071fa87f36ee8cd93a80db063
Adam Doyle <dominofoe@gmail.com>
2015-03-11 21:57:38 -0400

    dammit

--- js/script.js
+++ js/script.js
@@ -2,23 +2,28 @@ $(document).ready(function() {
 
 	toggle = 2;
 
+	
+	
+
 $("#left-title").on('click', function(event) {
 	event.preventDefault();
 
 	if (toggle != 1) {
-	$("#slide-selectors").css({
-		'text-align': 'right'
-	});	
+	
 	$("#youtube-results").css({
 		display: 'none'
 	});
-	$("#suggested-media").show("slide", 1000);
+	$(".border-left").show("slide", { direction: 'left', easing: 'easeInCirc' }, 1000);
+	$("#suggested-media").show("slide", { direction: 'left', easing: 'easeInCirc' }, 1000);
 	$("#suggested-media").css({
 		display: '-webkit-box',
 		display: '-moz-box',
 		display: '-ms-flexbox',
 		display: '-webkit-flex',
 		display: 'flex'
+	});
+	$(".more").css({
+		display: 'block'
 	});
 	
 	toggle = 1;
@@ -30,14 +35,12 @@ $("#right-title").on('click', function(event) {
 	event.preventDefault();	
 
 	if (toggle != 0) {
-
-	$("#slide-selectors").css({
-		'text-align': 'left'
-	});
+	
+	$(".border-right").show("slide", { direction: 'left', easing: 'easeInCirc' }, 1000);
 	$("#suggested-media").css({
 		display: 'none'
 	});
-	$("#youtube-results").show("slide", 1000);
+	$("#youtube-results").show("slide", { direction: 'right', easing: 'easeInCirc'}, 1000);
 	$("#youtube-results").css({
 		display: '-webkit-box',
 		display: '-moz-box',
@@ -45,6 +48,10 @@ $("#right-title").on('click', function(event) {
 		display: '-webkit-flex',
 		display: 'flex'
 	});
+	$(".more").css({
+		display: 'block'
+	});
+
 	
 	toggle = 0;
 }
