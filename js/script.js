59a3752fc896fbf81849d5abea04e41e9c070da8
Adam Doyle <dominofoe@gmail.com>
2015-03-12 00:36:49 -0400

    all non-functional JS complete

--- js/script.js
+++ js/script.js
@@ -1,63 +1,57 @@
 $(document).ready(function() {
 
-	toggle = 2;
-
-	
-	
-
-$("#left-title").on('click', function(event) {
-	event.preventDefault();
-
-	if (toggle != 1) {
-	
-	$("#youtube-results").css({
-		display: 'none'
-	});
-	$(".border-left").show("slide", { direction: 'left', easing: 'easeInCirc' }, 1000);
-	$("#suggested-media").show("slide", { direction: 'left', easing: 'easeInCirc' }, 1000);
-	$("#suggested-media").css({
-		display: '-webkit-box',
-		display: '-moz-box',
-		display: '-ms-flexbox',
-		display: '-webkit-flex',
-		display: 'flex'
-	});
-	$(".more").css({
-		display: 'block'
-	});
-	
-	toggle = 1;
-}
+SlideEffect();
 
 });
 
-$("#right-title").on('click', function(event) {
-	event.preventDefault();	
+	function SlideEffect() {
+		toggle = 2;	
 
-	if (toggle != 0) {
-	
-	$(".border-right").show("slide", { direction: 'left', easing: 'easeInCirc' }, 1000);
-	$("#suggested-media").css({
-		display: 'none'
-	});
-	$("#youtube-results").show("slide", { direction: 'right', easing: 'easeInCirc'}, 1000);
-	$("#youtube-results").css({
-		display: '-webkit-box',
-		display: '-moz-box',
-		display: '-ms-flexbox',
-		display: '-webkit-flex',
-		display: 'flex'
-	});
-	$(".more").css({
-		display: 'block'
-	});
+		$("#left-title").on('click', function(event) {
+			event.preventDefault();
 
-	
-	toggle = 0;
-}
+			if (toggle != 1) {	
+				$("#youtube-results").css({
+					display: 'none'
+				});
+				$("#left-title").addClass('sactive');
+				$("#right-title").removeClass('sactive')	;			
+				$("#suggested-media").show("slide", { direction: 'left', easing: 'easeInCirc' }, 1000);
+				$("#suggested-media").css({
+					display: '-webkit-box',
+					display: '-moz-box',
+					display: '-ms-flexbox',
+					display: '-webkit-flex',
+					display: 'flex'
+				});
+				$(".more").css({
+					display: 'block'		
+				});
+				toggle = 1;
+			}
+		});
 
-});
+		$("#right-title").on('click', function(event) {
+			event.preventDefault();	
 
-
-	
-});
+			if (toggle != 0) {					
+				$("#suggested-media").css({
+					display: 'none'
+				});	
+				$("#right-title").addClass('sactive');
+				$("#left-title").removeClass('sactive')				
+				$("#youtube-results").show("slide", { direction: 'right', easing: 'easeInCirc'}, 1000);
+				$("#youtube-results").css({
+					display: '-webkit-box',
+					display: '-moz-box',
+					display: '-ms-flexbox',
+					display: '-webkit-flex',
+					display: 'flex'
+				});
+				$(".more").css({
+					display: 'block'
+				});	
+				toggle = 0;
+			}
+		});
+	}
