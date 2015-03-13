47604f7cd8c3f446c1a7880323763c9529e6c8b3
Adam Doyle <dominofoe@gmail.com>
2015-03-13 00:35:57 -0400

    adding YT

--- js/script.js
+++ js/script.js
@@ -1,33 +1,44 @@
 $(document).ready(function() {
 
-	SlideEffect();
-
-	GetSearchTerms();
+	
+	Main();
+	
 
 	
 
 });
 
+function Main() {
+	GetSearchTerms();
 
 
+}
 
-/*----------Get user input---------------*/
+
+/*----------------Get user input-----------------------*/
 function GetSearchTerms() {
 	$("#search-find").on("click", function(event) {
 		event.preventDefault();
+		$("#suggested-media").empty();
+		type = $("#select-type option:selected").text();
 		terms = $("#search").val();
-		GetRequest(terms);
+		GetTkRequest(terms, type);
+		$("#search").val('');
+		console.log(type + '-' + terms);
+		
 	});
+	
 }
 
 /*----------------Get API Data------------------------*/
-var GetRequest = function(searchTerms){
+var GetTkRequest = function(searchTerms, selectedType){
 	var params = {
 		q: searchTerms,
+		type: selectedType,
 		info: 1		
 	};
 	url = "http://www.tastekid.com/api/similar";
-
+	
 	var result = $.ajax({
       		url: url,
       		data: params,
@@ -35,82 +46,73 @@ var GetRequest = function(searchTerms){
     	})
 
     	.done(function(result) {
-		//console.log(result);
+    		console.log(result);		
 		$.each(result.Similar.Results, function(i, item) {
-			var resultItem = ShowResults(item);
-			$('#results').append(resultItem);
-			//console.log(resultItem);
-		});
-	})
-
-	
-}
-
-function ShowResults(results) {
-	//console.log(results);
-	// clone our result template code
-	var result = $('.templates #suggested-media').clone();
-
-	var resultTitle = result.find('.result-item-title');
-	resultTitle.html('<p>' + results.Name + '</p>')
-	console.log(result);
-	//return result;
+			var resultTkItem = ShowTkResults(item);
+			var resultYtItem = GetYtRequest(item);
+			$('#suggested-media').append(resultTkItem);			
+		});		
+		SlideEffect();		
+	});	
+    	console.log(url);
 };
 
+function ShowTkResults(results) {	
+	// clone our result template code
+	var result = $('.templates .flex-item').clone();
+
+	var resultItemImage = result.find('img');
+
+
+	var resultItemTitle = result.find('p.result-item-title');
+	resultItemTitle.html(results.Name)
+
+	var resultItemType = result.find('p.result-item-type');
+	resultItemType.html(results.Type)
+
+	return result;
+
+};
+
+function GetYtRequest(tkData) {
+	var videoId = tkData.yID
+	var params = {
+    		part: 'snippet',
+    		key: 'AIzaSyDM9phtsd2BSBMrzDcwTDy-AE51tZO-qr8',
+    		q: videoId,
+    		maxResults:'1'
+    		};
+  		url = 'https://www.googleapis.com/youtube/v3/search';
+
+		$.getJSON(url, params, function(data){
+			console.log(data);
+			dataReturn = data;
+			ShowYtResults(data);
+
+		});
+	}
 
 
 
+function ShowYtResults() {
 
+
+}
 
 /*-----------------------Slide Animations----------------------*/
-function SlideEffect() {
-	toggle = 2;	
-
-	$("#left-title").on('click', function(event) {
-		event.preventDefault();
-
-		if (toggle != 1) {	
-			$("#youtube-results").css({
-				display: 'none'
-			});
-			$("#left-title").addClass('sactive');
-			$("#right-title").removeClass('sactive')	;			
-			$("#suggested-media").show("slide", { direction: 'left', easing: 'easeInCirc' }, 1000);
-			$("#suggested-media").css({
-				display: '-webkit-box',
-				display: '-moz-box',
-				display: '-ms-flexbox',
-				display: '-webkit-flex',
-				display: 'flex'
-			});
-			$(".more").css({
-				display: 'block'		
-			});
-			toggle = 1;
-		}
+function SlideEffect() {			
+	
+	$("#suggested-media").show("slide", { direction: 'left', easing: 'easeInCirc' }, 1000);
+	$("#suggested-media").css({
+		display: '-webkit-box',
+		display: '-moz-box',
+		display: '-ms-flexbox',
+		display: '-webkit-flex',
+		display: 'flex'
 	});
-
-	$("#right-title").on('click', function(event) {
-		event.preventDefault();	
-
-		if (toggle != 0) {					
-			$("#suggested-media").css({
-				display: 'none'
-			});	
-			$("#right-title").addClass('sactive');
-			$("#left-title").removeClass('sactive')				
-			$("#youtube-results").show("slide", { direction: 'right', easing: 'easeInCirc'}, 1000);
-			$("#youtube-results").css({
-				display: '-webkit-box',
-				display: '-moz-box',
-				display: '-ms-flexbox',
-				display: '-webkit-flex',
-				display: 'flex'
-			});
-			$(".more").css({
-				display: 'block'
-			});	
-			toggle = 0;
-		}
+	$(".more").css({
+		display: 'block'		
 	});
-}
+};		
+
+	
